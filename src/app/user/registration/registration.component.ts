fac3a1fb88678746f049dc9e34b1c799948ebd8f
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-02-28 11:35:30 -0600

    showing required error

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,24 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './registration.component.html',
   styles: []
 })
 
 export class RegistrationComponent implements OnInit {
-registrationForm: FormGroup;
+registrationForm: any;
 
 constructor(public formBuilder: FormBuilder) {
   this.registrationForm = this.formBuilder.group({
-    fullName: [''],
-    email: [''],
-    password: [''],
+    fullName: ['', Validators.required],
+    email: ['',[Validators.required,Validators.email]],
+    password: ['',[Validators.required, Validators.minLength(6), Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]],
     confirmPassword: ['']
-  });
+  },{validators: this.passwordMatchValidator});
 }
+
+  passwordMatchValidator:ValidatorFn = (control: AbstractControl):null => {
+    const password = control.get('password')
+    const confirmPassword = control.get('confirmPassword');
+    (password && confirmPassword && password.value != confirmPassword.value)  ? 
+    confirmPassword?.setErrors({passwordMismatch: true}) : confirmPassword?.setErrors(null);
+    return null;
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
