c0bf940d53be27360dc93b165fd56df3e67b9d0a
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-03-01 02:06:32 -0600

    created formbuilder

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styles: ``
 })
 export class LoginComponent {
-
+  loginForm: any;
+  constructor(public formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  
 }
