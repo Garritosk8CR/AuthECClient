10122096ce1c554775f4e08289598455ada29c11
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-02-28 20:30:09 -0600

    create post method createUser

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  baseUrl = 'http://localhost:5023/api';
+  createUser(model: any) {
+    return this.http.post(`${this.baseUrl}/signup`, model);
+  }
 }
