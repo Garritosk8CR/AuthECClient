78bc8eb9be1854db4100fcb2e73a2ee9e0e303e2
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-03-02 00:49:58 -0600

    spelling error fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class DashboardComponent {
 
   constructor(private router: Router) { }
-  onlogout() {
+  onLogout() {
     localStorage.removeItem('token');
     this.router.navigateByUrl('/signin');
   }
